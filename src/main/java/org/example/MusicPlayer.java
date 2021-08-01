0b4cb20126f772a01841c72ab0134c2558d33219
djonn79 <zheka.zaicev@gmail.com>
2021-08-02 09:37:35 +1000

    1

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+
+
+    public void playSong(){
+        System.out.println(music.getSong());
+    }
+}
---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Iterator;
+
+public class TestSpring {
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playSong();
+
+        context.close();
+    }
+
+}
