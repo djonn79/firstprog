ce6780b02fcc771f891444e076533ad96c37ce52
djonn79 <zheka.zaicev@gmail.com>
2021-08-02 17:43:32 +1000

    Added music list

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,15 +1,50 @@
 package org.example;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
+public class MusicPlayer {
+    private List<Music> music = new ArrayList<>();
+    private int volume;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MusicPlayer(List<Music> music) {
         this.music = music;
     }
 
+    public MusicPlayer(){}
 
+    public List<Music> getMusic() {
+        return music;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setMusic(List<Music> music) {
+        this.music = music;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public void playSong(){
-        System.out.println(music.getSong());
+        System.out.println("Volume is - " + volume);
+        System.out.println("My name is " + name);
+        for (Music m : music) {
+            System.out.println("I'm playing " + m.getSong() + " music");
+        }
+
+
     }
 }
